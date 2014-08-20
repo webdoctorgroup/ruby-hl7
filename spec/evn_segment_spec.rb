ae9f5e871488f1b4dddcca240098c1bc0b7cc151
Rob Dawson <robert@rojotek.com>
2014-08-20 20:09:52 +1000

    spec that verifies dates set are parsed correctly.

--- spec/evn_segment_spec.rb
+++ spec/evn_segment_spec.rb
@@ -12,10 +12,12 @@ describe HL7::Message::Segment::EVN do
       evn.type_code.should == "A04"
     end
 
-    it 'allows creation of an OBX segment' do
+    it 'allows creation of an EVN segment' do
       evn = HL7::Message::Segment::EVN.new
       evn.event_facility="A Facility"
       evn.event_facility.should == 'A Facility'
+      evn.recorded_date = Date.new 2001,2,3
+      evn.recorded_date.should == "20010203"
     end
   end
 end
