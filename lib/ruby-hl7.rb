8220acb6135dc3771ee6ac4982f55c1f7a7790f1
Enrique Carlos Mogollan <emogollan@desk.com>
2016-07-26 16:35:09 -0700

    Bumping version to 1.2.0

--- lib/ruby-hl7.rb
+++ lib/ruby-hl7.rb
@@ -21,7 +21,7 @@ require 'stringio'
 require 'date'
 
 module HL7 # :nodoc:
-  VERSION = '1.1.0'
+  VERSION = '1.2.0'
   def self.ParserConfig
     @parser_cfg ||= { :empty_segment_is_error => true }
   end
