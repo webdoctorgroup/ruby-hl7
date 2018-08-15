c7369c083a8b1ef1ffca3f1673bb3cef7837bc97
Lyle Mullican <ldmullican@gmail.com>
2018-08-15 11:30:16 -0400

    Rename PID-12 from 'country_code' to 'county_code' per the specification, retaining an alias for backwards compatibility with a deprecation warning.

--- lib/segments/pid.rb
+++ lib/segments/pid.rb
@@ -21,7 +21,7 @@ class HL7::Message::Segment::PID < HL7::Message::Segment
   add_field :patient_alias
   add_field :race
   add_field :address
-  add_field :country_code
+  add_field :county_code
   add_field :phone_home
   add_field :phone_business
   add_field :primary_language
@@ -53,4 +53,16 @@ class HL7::Message::Segment::PID < HL7::Message::Segment
   add_field :strain
   add_field :production_class_code
   add_field :tribal_citizenship
+
+  def country_code
+    warn "DEPRECATION WARNING: PID-12 is defined as 'county_code'; "+
+         "the 'country_code' alias is retained for backwards compatibility only."
+    county_code
+  end
+
+  def country_code=(country_code)
+    warn "DEPRECATION WARNING: PID-12 is defined as 'county_code'; "+
+         "the 'country_code' alias is retained for backwards compatibility only."
+    self.county_code = country_code
+  end
 end
