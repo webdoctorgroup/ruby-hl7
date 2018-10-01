06c930a6e7228a995499abb40b31311027e1bc56
Raul Perez-Alejo Neyra <rperezalejo@openjaf.com>
2018-10-01 12:49:04 -0300

    adding all fields to the ft1 segment

--- spec/ft1_segment_spec.rb
+++ spec/ft1_segment_spec.rb
@@ -4,7 +4,7 @@ require 'spec_helper'
 describe HL7::Message::Segment::FT1 do
   context 'general' do
     before :all do
-      @base_ft1 = 'FT1|1|||20180705|20180705|||Description||1|||||||||R45.82^Worries~H18.892^Other specified disorders of cornea|1043312457^Provider^Testing^^^^|1043312457^Provider^Testing^^^^||||99392^HEART FAILURE COMPOSITE|'
+      @base_ft1 = 'FT1|1|||20180705|20180705|||Description||1||||||123456^The Location|||R45.82^Worries~H18.892^Other specified disorders of cornea|1043312457^Provider^Testing^^^^|1043312457^Provider^Testing^^^^||||99392^HEART FAILURE COMPOSITE|'
     end
 
     it 'creates an FT1 segment' do
@@ -23,6 +23,7 @@ describe HL7::Message::Segment::FT1 do
         expect(ft1.transaction_posting_date).to eq '20180705'
         expect(ft1.transaction_description).to eq 'Description'
         expect(ft1.transaction_quantity).to eq '1'
+        expect(ft1.assigned_patient_location).to eq '123456^The Location'
         expect(ft1.diagnosis_code).to eq 'R45.82^Worries~H18.892^Other specified disorders of cornea'
         expect(ft1.performed_by_provider).to eq '1043312457^Provider^Testing^^^^'
         expect(ft1.ordering_provider).to eq '1043312457^Provider^Testing^^^^'
